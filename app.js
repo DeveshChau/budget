cfb36ac019d38ae47a49f22de50f10c672e51f21
DeveshChau <deveshachaudhari@gmail.com>
2020-08-07 12:06:22 +0530

    add income expense item

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const UIController = (function () {
         add__type: '.add__type',
         add__description: '.add__description',
         add__value: '.add__value',
-        add__btn: '.add__btn'
+        add__btn: '.add__btn',
+        income__list: '.income__list',
+        expenses__list: '.expenses__list'
     };
     return {
         getInput: function () {
@@ -13,6 +15,38 @@ const UIController = (function () {
                 value: document.querySelector(DOMString.add__value).value
             }
         },
+        addItemList: function (newItem, type) {
+            let element;
+            if (type === 'inc') {
+                element = DOMString.income__list;
+                html = `<div class="item clearfix" id="income-%id%">
+                <div class="item__description">%description%</div>
+                <div class="right clearfix">
+                    <div class="item__value">%value%</div>
+                    <div class="item__delete">
+                        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                    </div>
+                </div>
+            </div>`;
+            } else if (type === 'exp') {
+                element = DOMString.expenses__list;
+                html = `<div class="item clearfix" id="expense-%id%">
+                <div class="item__description">%description%</div>
+                <div class="right clearfix">
+                    <div class="item__value">%value%</div>
+                    <div class="item__percentage">21%</div>
+                    <div class="item__delete">
+                        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                    </div>
+                </div>
+            </div>`;
+            }
+
+            newHTML = html.replace('%id%', newItem.ID);
+            newHTML = newHTML.replace('%description%', newItem.description);
+            newHTML = newHTML.replace('%value%', newItem.value);
+            document.querySelector(element).insertAdjacentHTML("beforeend", newHTML)
+        },
         getDOMString: function () {
             return DOMString;
         }
@@ -20,7 +54,35 @@ const UIController = (function () {
 })();
 
 const DataController = (function () {
-
+    
+    let data = {
+        item: {
+            inc: [],
+            exp: []
+        }
+    }
+    let Income = function (id, description, value) {
+        this.ID = id,
+            this.description = description,
+            this.value = value
+    }
+    return {
+        addItem: function (type, description, value) {
+            let ID, newItem
+            if (data.item[type].length > 0) {
+                ID = data.item[type][data.item[type].length - 1].id + 1;
+            } else {
+                ID = 0
+            }
+            if (type === 'inc') {
+                newItem = new Income(ID, description, value);
+            } else if (type === 'exp') {
+                newItem = new Income(ID, description, value);
+            }
+            data.item[type].push(newItem);
+            return newItem;
+        }
+    }
 })();
 
 const Controller = (function (uiCtrl, dataCtrl) {
@@ -33,12 +95,13 @@ const Controller = (function (uiCtrl, dataCtrl) {
             }
         });
     }
-    const ctrlAddItems = function() {
+    const ctrlAddItems = function () {
         const input = uiCtrl.getInput();
-        console.log(input);        
+        newItem = dataCtrl.addItem(input.type, input.description, input.value);
+        uiCtrl.addItemList(newItem, input.type);
     }
     return {
-        init: function() {
+        init: function () {
             console.log('Application started.')
             setUpEventListener();
         }
